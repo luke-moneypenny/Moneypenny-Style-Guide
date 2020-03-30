b0c54d8285d51f6f5147c8f77036f04127211275
Luke Turnbull <luke@moneypenny.co.uk>
2020-03-30 16:39:11 +0100

    Buttons

--- handlebarsPackages.js
+++ handlebarsPackages.js
@@ -1,15 +1,20 @@
 window.onload = function() {
-  var hbColoursTemplate = document.getElementById('hbColoursTemplate').innerHTML;
   var compiled_hbColoursTemplate = Handlebars.compile(hbColoursTemplate);
   var rendered_hbColoursTemplate = compiled_hbColoursTemplate({
 
   	colour: [
+ 		{ var: "$orange", hex: "#FF5039" },
+ 		{ var: "$green", hex: "#38FE9E" },
+ 		{ var: "$blue", hex: "#8ABFFF" },
+ 		{ var: "$purple", hex: "#271833" },
  		{ var: "$white", hex: "#FFFFFF" },
  		{ var: "$black", hex: "#000000" },
- 		{ var: "$blue", hex: "#509bf4" },
- 		{ var: "$orange", hex: "#FF5039" },
  	]
 
   });
   document.getElementById('HBcolours').innerHTML = rendered_hbColoursTemplate;
 }
+
+/* Templates */
+
+var hbColoursTemplate = '{{#each colour}}<div class="colourPod"><div class="colourPod__top" style="border-bottom: solid 5px {{this.hex}};"><h2>{{this.var}}</h2><p>{{this.hex}}</p></div><div class="colourPod__white"><p class="fnt-s" style="color:{{this.hex}};">Small font</p><p class="fnt-m" style="color:{{this.hex}};">Med font</p><p class="fnt-l" style="color:{{this.hex}};">Large font</p><p class="fnt-xl" style="color:{{this.hex}};">Extra large font</p></div><div class="colourPod__whitetext" style="background-color: {{this.hex}};"><p class="fnt-s">Small font</p><p class="fnt-m">Med font</p><p class="fnt-l">Large font</p><p class="fnt-xl">Extra large font</p></div><div class="colourPod__darktext" style="background-color: {{this.hex}};"><p class="fnt-s">Small font</p><p class="fnt-m">Med font</p><p class="fnt-l">Large font</p><p class="fnt-xl">Extra large font</p></div></div>{{/each}}';
